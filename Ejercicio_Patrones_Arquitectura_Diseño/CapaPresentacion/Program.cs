0414f7e4efabac3f7375e787ed97e4e53274750c
nachozbrun <ignazbrun5@gmail.com>
2022-04-12 13:48:48 -0300

    Falta ultimo punto de usar el patron de cascada para enviar mail

--- Ejercicio_Patrones_Arquitectura_Diseño/CapaPresentacion/Program.cs
+++ Ejercicio_Patrones_Arquitectura_Diseño/CapaPresentacion/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Ingrese su DNI");
+int dni = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el codigo de la enfermedad");
+int codigoEnfermedad = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la fecha de atencion");
+DateTime fechaAtencion = DateTime.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese su mail");
+string mail = Console.ReadLine();
+CapaLogicaNegocio.Singleton.Instance.CargarAtenciones(dni, codigoEnfermedad, fechaAtencion, para);
+
+
+
+
