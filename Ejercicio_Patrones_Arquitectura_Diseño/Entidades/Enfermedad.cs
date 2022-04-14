a7ff7d0f88573dd6092f8cfe81c175a88ad52cbf
nachozbrun <ignazbrun5@gmail.com>
2022-04-13 23:50:34 -0300

    Practica terminada

---
+++ Ejercicio_Patrones_Arquitectura_Diseño/Entidades/Enfermedad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Enfermedad
+    {
+        public int CodigoEnfermedad;
+        public tipoEnfermedad TipoEnfermedad { get; set; }
+        public string Nombre { get; set; }
+        public double CostoAsociado { get; set; }
+
+        public Enfermedad() { }
+        public Enfermedad(int codigoEnfermedad, tipoEnfermedad tipoEnfermedad, string nombre, double costoAsociado)
+        {
+            this.CodigoEnfermedad = codigoEnfermedad;
+            this.TipoEnfermedad = tipoEnfermedad;
+            this.Nombre = nombre;
+            this.CostoAsociado = costoAsociado;
+        }
+    }
+}
